7c610f0443a8abf0a380941bbb33887d6b0d4de3
asylmkm <asylmkm@gmail.com>
2020-06-21 02:22:38 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,33 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  const hasDirectory = pathname.split('/').length - 1;
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+      if (hasDirectory) {
+        res.statusCode = 400;
+        res.end('Path has directories');
+      } else {
+        fs.createReadStream(filepath).on('error', (error) => {
+          if (error.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('File not found');
+          } else {
+            res.statusCode = 500;
+            res.end('Server error');
+          }
+        })
+            .pipe(res);
+      }
       break;
 
     default:
